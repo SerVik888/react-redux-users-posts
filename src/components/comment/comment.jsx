8becbb54bd10113aac214bcd24c893f037ebb2be
SerVik888 <genom.86@inbox.ru>
2022-05-27 11:32:23 +0300

    comm

--- src/components/comment/comment.jsx
+++ src/components/comment/comment.jsx
@@ -3,8 +3,7 @@ import s from './comment.module.css'
 
 export const Comment = ({ comment }) => {
   return (
-    <div>
-      <hr />
+    <div className={s.comment_wrapper}>
       <h5 className={s.comment_title}>{comment.name}</h5>
       <p className={s.comment_email}>
         <b>{comment.email}</b>
